0c2a5b0a5151e0ecf9e246dd3da62bd108101eb2
GoldSpaghetti <alan.chen.2k6@gmail.com>
2024-02-03 01:49:48 +0800

    a

--- Assets/Scripts/FishManager.cs
+++ Assets/Scripts/FishManager.cs
@@ -9,13 +9,14 @@ public class FishManager : MonoBehaviour
     public List<Fish> allFish = new List<Fish>();
     public GameObject fish;
     public float radius;
+    public int boundary;
     void Start()
     {
         GenerateFish();
-        foreach (GameObject fishGameObject in GameObject.FindGameObjectsWithTag("fish")){
-            allFish.Add(fishGameObject.GetComponent<Fish>());
-            Debug.Log("found fish: " + fishGameObject.name);
-        }
+        // foreach (GameObject fishGameObject in GameObject.FindGameObjectsWithTag("fish")){
+        //     allFish.Add(fishGameObject.GetComponent<Fish>());
+        //     Debug.Log("found fish: " + fishGameObject.name);
+        // }
     }
 
     // Update is called once per frame
@@ -31,32 +32,32 @@ public class FishManager : MonoBehaviour
                 
             // }
             Vector3 newVector = fish.transform.position;
-            if (fish.transform.position.x <=-6){
-                newVector.x = 6;
+            if (fish.transform.position.x <=-boundary){
+                newVector.x = boundary;
             }
-            if (fish.transform.position.x > 6){
-                newVector.x = -6;
+            if (fish.transform.position.x > boundary){
+                newVector.x = -boundary;
             }
-            if (fish.transform.position.y <=-6){
-                newVector.y = 6;
+            if (fish.transform.position.y <=-boundary){
+                newVector.y = boundary;
             }
-            if (fish.transform.position.y > 6){
-                newVector.y = -6;
+            if (fish.transform.position.y > boundary){
+                newVector.y = -boundary;
             }
-            if (fish.transform.position.z <=-6){
-                newVector.z = 6;
+            if (fish.transform.position.z <=-boundary){
+                newVector.z = boundary;
             }
-            if (fish.transform.position.z > 6){
-                newVector.z = -6;
+            if (fish.transform.position.z > boundary){
+                newVector.z = -boundary;
             }
             fish.transform.position = newVector;
             
         }
     }
     void GenerateFish(){
-        for (int i = -6; i < 6; i+=2){
-            for (int j = -6; j < 6; j+=2){
-                for (int k = -6; k < 6; k+=2){
+        for (int i = -boundary; i < boundary; i+=2){
+            for (int j = -boundary; j < boundary; j+=2){
+                for (int k = -boundary; k < boundary; k+=2){
                     Instantiate(fish, new Vector3(i, j, k), UnityEngine.Random.rotation);
                 }
             }
